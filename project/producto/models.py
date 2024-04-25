b96c6a551ca532022f50326ca1bebb4634e14512
RAArrieta <alfredoarrieta99@gmail.com>
2024-04-25 13:45:05 -0300

    Creacion de Productos

---
+++ project/producto/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+class Categoria(models.Model):
+
+    nombre = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nombre
+
+class Producto(models.Model):
+    nombre = models.CharField(max_length=200)
+    descripcion = models.CharField(max_length=200)
+    precio = models.IntegerField()
+    categoria_id = models.ForeignKey(Categoria, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return f"{self.nombre} {self.precio}"
