e76b4795603ca3bc3a4ef63eb205824de4debed0
RAArrieta <alfredoarrieta99@gmail.com>
2024-04-25 18:12:29 -0300

    bootstrap

--- project/producto/urls.py
+++ project/producto/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'producto'
+
 urlpatterns = [
-    path('', views.home, name = "home"),
+    path('home/', views.home, name='home'),
 ]
