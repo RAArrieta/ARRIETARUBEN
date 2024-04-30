de6869643a4556bd2a252d26d23c8664d2cb75e2
RAArrieta <alfredoarrieta99@gmail.com>
2024-04-30 20:21:15 -0300

    Class Nacionalidad

--- project/cliente/admin.py
+++ project/cliente/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
 from . import models
 
+admin.site.register(models.Nacionalidad)
 admin.site.register(models.Cliente)
+
--- project/cliente/models.py
+++ project/cliente/models.py
@@ -1,12 +1,18 @@
 from django.db import models
 
+class Nacionalidad(models.Model):
+    nombre = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.nombre}"
+
 class Cliente(models.Model):
     nombre = models.CharField(max_length=50)
     apellido = models.CharField(max_length=50)
     telefono = models.IntegerField()
     mail = models.CharField(max_length=50, blank=True, null=True)
-    nacionalidad = models.CharField(max_length=100, blank=True, null=True)
     direccion = models.CharField(max_length=100, blank=True, null=True)
+    nacionalidad_id =models.ForeignKey(Nacionalidad, on_delete=models.SET_NULL, blank=True, null=True)
     
     def __str__(self):
         return f"{self.nombre} {self.apellido}"
