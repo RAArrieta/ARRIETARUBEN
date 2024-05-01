aaa082a6663cfcc3b371992f6a6419ffc5f6c0dc
RAArrieta <alfredoarrieta99@gmail.com>
2024-04-30 21:02:42 -0300

    create nacionalidad

--- project/cliente/forms.py
+++ project/cliente/forms.py
@@ -1,6 +1,14 @@
 from django import forms
 from . import models
 
+class NacionalidadForm(forms.ModelForm):
+    class Meta:
+        model = models.Nacionalidad
+        fields = "__all__"
+        widgets = {
+            "nombre": forms.TextInput(attrs={"class": "form-control", "placeholder": "Nombre"}),
+        }
+
 class ClienteForm(forms.ModelForm):
     class Meta:
         model = models.Cliente
